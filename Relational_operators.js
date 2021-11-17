307e4cba3e0e67d1e55b20679ea9219761804958
Gokul kumar muvvala <62056446+gokulmuvvala@users.noreply.github.com>
2021-11-17 21:06:22 +0530

    Add files via upload

---
+++ Relational_operators.js
@@ -0,0 +1,27 @@
+ // Considering x and y are variables
+
+ var x = 6
+ var y = 4
+
+ // normal comparison
+ console.log(x==y)
+ // Js comparison
+ console.log(x===y) // === Here first we are comparing the data_types of variables and next variables
+// greaterthan or equal
+ console.log(x>=y)
+// lesserthan or equal
+ console.log(x<=y)
+// Notequal
+ console.log(x!=y)
+
+var x = true
+var y = true
+
+console.log(x==y)
+ // Js comparison
+ console.log(x===y) // === Here first we are comparing the data_types of variables and next variables
+
+ var x= "gokul"
+ var y= "ravi"
+// js string comparison
+ console.log(x===y) 
