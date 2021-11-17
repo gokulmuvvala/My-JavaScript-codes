5942099b71136284a1e2ef0d9f1e6825b30d676a
Gokul kumar muvvala <62056446+gokulmuvvala@users.noreply.github.com>
2021-11-17 20:20:06 +0530

    Add files via upload

---
+++ Datatypes_in_JS.js
@@ -0,0 +1,12 @@
+// In ths code we are just checking the data types of variables a,b & c.
+// integer
+var a = 2
+console.log(typeof a)
+
+// string
+var b = "Gokul"
+console.log(typeof b)
+
+// boolean
+var c = true
+console.log(typeof c)
