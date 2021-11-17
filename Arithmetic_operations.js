6d32037a563a6ff3951c4c364d762a808aa42fe8
Gokul kumar muvvala <62056446+gokulmuvvala@users.noreply.github.com>
2021-11-17 19:38:20 +0530

    Add files via upload
    I have done some Arithmetic operations in the JavaScript in the code.
    This all operations are performed
    +
    -
    *
    /
    %

---
+++ Arithmetic_operations.js
@@ -0,0 +1,22 @@
+/* Arithmetic operations in Java script
+Here we are considering (a) and (b) are variables */
+
+// Creating the a & b variables
+var a = 24
+var b = 16
+
+// Addition
+console.log(a+b); // 40
+
+// Difference between a & b or subtraction
+console.log(a-b); // 8
+
+// Product
+console.log(a*b); // 384
+
+// division
+console.log(a/b); // 1.5
+
+// Remainder between a and b (or) Modulo
+console.log(a%b); // 8
+
