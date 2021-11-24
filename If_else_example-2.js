35bdc98f97bc4fe20a7ebc6088e084b44a77d917
Gokul kumar muvvala <62056446+gokulmuvvala@users.noreply.github.com>
2021-11-24 15:28:45 +0530

    Add files via upload

---
+++ If_else_example-2.js
@@ -0,0 +1,8 @@
+// Creating 3 variables
+let num1 = 25, num2 = 4, num3 = 19;
+if(num1 >num2 && num1 >num3){
+    console.log("num1 is Greater than num2 and num3");
+}
+else{
+    console.log("num1 is lesser than num2 and num3")
+}
---
+++ else if example.js
@@ -0,0 +1,11 @@
+// Creating 3 variables
+let num1 = 25, num2 = 4, num3 = 19;
+if(num1 > num2 && num1 > num3){
+    console.log("num1 is Greater than num2 and num3");
+}
+else if(num2 > num3) {
+    console.log("num2 is Greater than num1 and num3")
+}
+else{
+    console.log("num3 is greaterthan num1 and num2 ")
+}
