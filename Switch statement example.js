102ea1e92bf78ae6576affb26d7667c9fb8546a1
Gokul kumar muvvala <62056446+gokulmuvvala@users.noreply.github.com>
2021-11-24 17:38:39 +0530

    Add files via upload

---
+++ Switch statement example.js
@@ -0,0 +1,37 @@
+// Switch statement example
+// Remainders for next week
+
+/*Monday: I should take my car for servicing at 10:00 AM
+Tuesday: Complete the work regarding the New video Youtube at 5:00 PM
+Wednesday: Complete the collage assignments at 9:30 AM
+Thursday: Driving class at 4:00 PM 
+Friday: Buy groceries from market
+Saturday: Week-off no work
+Sunday: Upload new video in channel at 9:00 PM */
+
+/*This are my schedule for next week*/
+let Day = "Monday" // This is day we need to check  
+
+switch(Day){
+    case "Monday":
+        console.log("I should take my car for servicing at 10:00 AM");
+        break;
+    case "Tuesday":
+        console.log("Complete the work regarding the New video Youtube at 5:00 PM");
+        break;
+    case "Wednesday":
+        console.log("Complete the collage assignments at 9:30 AM");
+        break;
+    case "Thursday":
+        console.log("Driving class at 4:00 PM ");
+        break;
+    case "Friday":
+        console.log("Buy groceries from market");
+        break;
+    case "Saturday":
+        console.log("Week-off no work");
+        break;
+    case "Sunday":
+        console.log("Upload new video in channel at 9:00 PM");
+        break;
+}
