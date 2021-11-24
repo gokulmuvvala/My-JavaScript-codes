a1758065bd8e0e535e800ffbf656606a71a501db
Gokul kumar muvvala <62056446+gokulmuvvala@users.noreply.github.com>
2021-11-24 11:34:06 +0530

    Add files via upload

---
+++ Logical_operations.js
@@ -0,0 +1,8 @@
+let x = 5 , y = 24 , z = 55
+
+let result = x > y && z < y // And operation 
+let result1 = x < y || z < y // Or operation
+let n = !result // Not operation
+console.log(result);
+console.log(result1);
+console.log(n);
