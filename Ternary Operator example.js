8ad97b5ac75de95c0530d30b0ef3c79d3dc42bc5
Gokul kumar muvvala <62056446+gokulmuvvala@users.noreply.github.com>
2021-11-24 15:42:33 +0530

    Add files via upload

---
+++ Ternary Operator example.js
@@ -0,0 +1,15 @@
+// Ternary operator example
+
+let num = 4
+let result
+
+// General method
+// if(num%2===0)
+//     result ="Even"
+// else
+//     result = "Odd"
+
+// Using ternary method (?)
+
+result = num%2 ===0 ? "Even" : "Odd"
+console.log(result);
