957f76380cc859181351926835f0f78ec902af29
Gokul kumar muvvala <62056446+gokulmuvvala@users.noreply.github.com>
2021-11-28 14:00:20 +0530

    Add files via upload

---
+++ Nested While loop example-2.js
@@ -0,0 +1,14 @@
+// Nested While loop example-2
+let i=2,j=3,k=1; 
+while(i<=4){
+    console.log("Welcome Gokul",i);
+    i++;
+    while(j<=6){
+        console.log("Thankyou",j);
+    j++;
+    }
+    while(k<=7){
+        console.log("pumpkin",k);
+    k++;    
+    }
+}
