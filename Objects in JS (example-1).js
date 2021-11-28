355081c0df7015d436895bd27a298267ec2a8c93
Gokul kumar muvvala <62056446+gokulmuvvala@users.noreply.github.com>
2021-11-28 16:19:02 +0530

    Add files via upload

---
+++ Objects in JS (example-1).js
@@ -0,0 +1,11 @@
+// Objects in JS (example-1)
+
+// Creating a Object
+Laptop = {
+    // Declaring the object variables 
+    name: "Dell XPS 40",
+    Processor: "I5 8th Gen"
+}
+
+// Printing the object.
+console.log(Laptop);
---
+++ Objects in JS (example-2).js
@@ -0,0 +1,13 @@
+// Objects in JS (example-2)
+
+// Creating a Object
+Laptop = {
+    // Declaring the object variables 
+    name: "Dell XPS 40",
+    Processor: "I5 8th Gen"
+}
+
+// Printing the specific properties in an object
+console.log(Laptop.name); // with help of (.) operator
+                 // OR
+console.log(Laptop['Processor']); // using the ([]) operator // [here we can use both both "" and '']
