efca687612b8928bbf89b4edf6f2f569ef9d4158
Gokul kumar muvvala <62056446+gokulmuvvala@users.noreply.github.com>
2021-12-02 11:45:02 +0530

    Add files via upload

---
+++ Methods in java script example-1.js
@@ -0,0 +1,15 @@
+// Methods example in js
+
+// creating an object 
+let laptop={
+    cpu:"i9",
+    ram:"16gb",
+    brand:"DEll",
+    // Declaring a Method inside the function
+    gokul:function(){
+        console.log("hello world");
+        console.log("hello humans");
+    } 
+}
+// calling the method
+laptop.gokul();
