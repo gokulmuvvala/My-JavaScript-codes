7b25dedce4743e44df0db3120ae155d93533ceb1
Gokul kumar muvvala <62056446+gokulmuvvala@users.noreply.github.com>
2021-12-04 08:21:12 +0530

    Add files via upload

---
+++ Arrays example-2.js
@@ -0,0 +1,9 @@
+// Arrays example-2
+// Pushing the elements into the array
+
+let toys =[]; // Empty arrays is created 
+
+toys.push(15); // pushing one element in to the array
+console.log(toys); // printing array elements
+toys.push(7,19); // pushing two the elements into the array
+console.log(toys); // printing array elements
