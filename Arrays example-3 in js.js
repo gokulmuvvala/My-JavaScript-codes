28c967c8deb0e4d55b9bd46ffafeeaa88f109a99
Gokul kumar muvvala <62056446+gokulmuvvala@users.noreply.github.com>
2021-12-04 12:07:02 +0530

    Add files via upload

---
+++ Arrays example-3 in js.js
@@ -0,0 +1,11 @@
+// Arrays example-3
+// Pushing the elements into the array
+
+let toys =[5,'Bob',{onplus:'Nord'},function(){
+console.log("Never settle")}]; // Array is created with added elements,object and function
+
+toys.push('Gokul'); // pushing one element in to the array
+console.log(toys); // printing array elements
+toys.push(7,'Rakesh'); // pushing two the elements into the array
+console.log(toys); // printing array elements
+toys[3](); // calling the function 
